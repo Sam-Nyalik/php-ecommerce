ad556c3d071b37d9b4a577e1ef1b910ced165bba
Sam-Nyalik <samnyalik@gmail.com>
2021-09-07 22:31:36 +0300

    Google Maps integration

--- js/main.js
+++ js/main.js
@@ -15,6 +15,20 @@ function passwordView() {
 }
 
 // Logout alert popup
-function logout(){
+function logout() {
   confirm("Are you sure you want to sign out?");
+}
+
+// Contact-Us Map
+function myMap() {
+  var map_properties = {
+    center: new google.maps.LatLng(-1.29738, 36.7849),
+    zoom: 5,
+    mapTypeId: google.maps.MapTypeId.HYBRID,
+  };
+
+  var map = new google.maps.Map(
+    document.getElementById("google_map"),
+    map_properties
+  );
 }
