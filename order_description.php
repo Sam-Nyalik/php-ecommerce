c602deedfa3e610e4ebdd77c8bace6f883023681
Sam-Nyalik <samnyalik@gmail.com>
2021-10-04 07:41:10 +0300

    Quick update

---
+++ order_description.php
@@ -0,0 +1,26 @@
+<?php 
+
+    // Start a session
+    session_start();
+    
+    // Check if the user is logged in
+    include_once "user/check_login.php";
+    include_once "functions/functions.php";
+    $pdo = databaseConnect();
+
+?>
+
+<!-- Header Template -->
+<?=headerTemplate('USER | ORDER-DESCRIPTION'); ?>
+
+<div class="container" style="margin-top: 45px;">
+    <div class="row">
+        <p><a class="text-secondary" href="index.php?page=home"><i class="bi bi-arrow-left-circle"></i> Home</a></p>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <h3 class="text-center">Your order has been received. Thank you!</h3>
+    </div>
+</div>
