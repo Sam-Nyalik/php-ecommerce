a4c5a79701bbc8a7d17d727467cd4eb5c0d14c2a
Sam-Nyalik <samnyalik@gmail.com>
2021-10-07 08:26:46 +0300

    Quick update

--- user/login.php
+++ user/login.php
@@ -17,8 +17,12 @@ include_once "functions/functions.php";
 $pdo = databaseConnect();
 
 // Google reCAPTCHA API key configuration
-$site_key = "6LeyX04cAAAAAOZiUSPypBh5G-wwyC1jozGbU1qc";
-$secret_key = "6LeyX04cAAAAAEhgmzA9eE_FRr-y6qzyqnXcoUnX";
+
+// Go to goggle's reCAPTCHA, and request for a site key
+$site_key = "";
+
+// Go to goggle's reCAPTCHA, and request for a secret key
+$secret_key = "";
 
 // Define variables and assign them empty values
 $email = $password = $recaptcha = "";
